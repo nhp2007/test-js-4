7cd7be057488811c53f81827fd9777fead3264f0
nhp2007 <165290650+nhp2007@users.noreply.github.com>
2024-06-08 21:20:51 +0530

    update

---
+++ test-4/index.js
@@ -0,0 +1,79 @@
+//  3
+
+
+// let arr1 =[1, 2, 3, 4, 5, 6, 8, 9, 10]
+// function sumcalculate (arr){
+//     let sum =0;
+//     for(let i =0;i<arr.length;i++){
+//         if(arr[i]%2 === 0){
+//             sum +=arr[i]
+//            
+//         }
+//     }
+//     return sum
+// }
+
+//   result=sumcalculate (arr1)
+// console.log(result)
+
+
+// 1
+
+// for(let i=0;i<=20;i++){
+//     if(i % 2 === 0 && i !== 10) {
+//     console.log(i)
+//     }
+// }
+
+// 8
+
+// const str1 = "javascript"
+
+// function reverseString (str){
+// let str2 = ""
+
+// for (let i=str1.length-1; i>=0;i--){
+//     str2+=str1[i]
+//     // console.log(str1[i])
+//     // str.push
+// }
+// return str2
+// }
+
+// const result = reverseString(str1)
+// console.log(result)
+
+// 5
+
+
+// const names =["virat","rahul","rohit","ravindra"]
+
+
+// function length2 (strings){
+//     const result =[]
+//     for(let i=0;i<strings.length;i++){
+//         if(strings[i].length > 7){
+//             result.push(strings[i])
+//         }
+//     }
+//     return result
+// }
+// const result2 = length2(names)
+
+// console.log(result2)
+
+// 7
+
+// function divisiblebynum(){
+//     let sum =0;
+//     for (let i=0;i<1000;i++){
+//         if(i%3===0 || i%5===0 || i%7===0){
+//             sum=sum+i
+//         }
+//     }
+//     return sum
+// }
+
+// const result = divisiblebynum()
+
+// console.log(result)
